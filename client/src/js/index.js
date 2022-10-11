efece46eea260b4dfbfa9a0e44c7089eb25786c6
Louie Moretti <morettilouie@gmail.com>
2022-10-11 05:09:38 -0500

    webpacked js css bootstrap and html

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,18 @@
+// javascript
 import "./form";
 import  "./submit";
+// images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+// css
+import "../css/index.css";
+//bootstrap
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+window.addEventListener('load', function() {
+    document.getElementById('logo').src = Logo;
+    document.getElementById('bearThumbnail').src = Bear;
+    document.getElementById('dogThumbnail').src = Dog;
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -7,10 +8,34 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    rules: [
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-        },
-    ]
+    plugins: [
+      new HtmlWebpackPlugin({
+          template: './index.html',
+          title: 'Webpack Plugin'
+      })
+    ],
+    module: {
+        rules: [
+            {
+              test: /\.(png|svg|jpg|jpeg|gif)$/i,
+              type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+          ]
+    }
 };
