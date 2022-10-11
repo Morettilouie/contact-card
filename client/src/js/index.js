6c64ac1d720e83049aa04e79a9e4228d5c29b97c
Louie Moretti <morettilouie@gmail.com>
2022-10-11 06:30:54 -0500

    made database

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,7 @@
 // javascript
 import "./form";
 import  "./submit";
+import { initdb } from "./database";
 // images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
@@ -12,6 +13,7 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 window.addEventListener('load', function() {
+    initdb()
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
