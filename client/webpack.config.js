fd79ec2f98c9a505407cacdc41b78c9b644ff835
Louie Moretti <morettilouie@gmail.com>
2022-10-12 06:36:42 -0500

    install

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {InjectManifest} = require('workbox-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
     mode: 'development',
@@ -10,20 +11,42 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      new HtmlWebpackPlugin({
-          template: './index.html',
-          title: 'Webpack Plugin'
-      }),
-      new InjectManifest({
-        swSrc: './src/sw.js',
-        swDest: 'service-worker.js'
-      })
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            title: 'Webpack Plugin'
+        }),
+        new InjectManifest({
+            swSrc: './src/sw.js',
+            swDest: 'service-worker.js'
+        }),
+        new WebpackPwaManifest({
+            name: 'Contact Cards Application',
+            short_name: 'Contact Cards',
+            description: 'Keep track of contacts!',
+            background_color: '#7eb4e2',
+            theme_color: '#7eb4e2',
+            start_url: './',
+            publicPath: './',
+            icons: [
+                {
+                    src: path.resolve('src/images/icon-manifest.png'),
+                    sizes: [96, 128, 192, 256, 384, 512],
+                    destination: path.join('assets', 'icons'),
+                },
+                {
+                    src: path.resolve('src/images/icon-manifest.png'),
+                    size: '1024x1024',
+                    destination: path.join('assets', 'icons'),
+                    purpose: 'maskable'
+                }
+            ],
+        })
     ],
     module: {
         rules: [
             {
-              test: /\.(png|svg|jpg|jpeg|gif)$/i,
-              type: 'asset/resource',
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
             {
                 test: /\.css$/i,
@@ -41,6 +64,6 @@ module.exports = {
                     }
                 }
             }
-          ]
+        ]
     }
 };
